d5fa7de1391716dbc8c8a62e892be92b0f00b8b9
eveporcello <eve@moonhighway.com>
2023-10-02 11:45:16 -0700

    adds initial repo with working subs

---
+++ index.js
@@ -0,0 +1,153 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import fs from "fs";
+import bodyParser from "body-parser";
+import { GraphQLScalarType } from "graphql";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
+import { PubSub } from "graphql-subscriptions";
+
+import lifts from "./data/lifts.json" assert { type: "json" };
+import trails from "./data/trails.json" assert { type: "json" };
+
+import "dotenv/config";
+
+const typeDefs = fs.readFileSync(
+  "./typeDefs.graphql",
+  "UTF-8"
+);
+const pubsub = new PubSub();
+const resolvers = {
+  Query: {
+    allLifts: (parent, { status }) =>
+      !status
+        ? lifts
+        : lifts.filter((lift) => lift.status === status),
+    findLiftById: (parent, { id }) =>
+      lifts.find((lift) => id === lift.id),
+    liftCount: (parent, { status }) =>
+      !status
+        ? lifts.length
+        : lifts.filter((lift) => lift.status === status)
+            .length,
+    allTrails: (parent, { status }) =>
+      !status
+        ? trails
+        : trails.filter((trail) => trail.status === status),
+    findTrailByName: (parent, { name }) =>
+      trails.find((trail) => name === trail.name),
+    trailCount: (parent, { status }) =>
+      !status
+        ? trails.length
+        : trails.filter((trail) => trail.status === status)
+            .length
+  },
+  Mutation: {
+    setLiftStatus: (parent, { id, status }) => {
+      let updatedLift = lifts.find(
+        (lift) => id === lift.id
+      );
+      updatedLift.status = status;
+      pubsub.publish("lift-status-change", {
+        liftStatusChange: updatedLift
+      });
+      return updatedLift;
+    },
+    setTrailStatus: (parent, { id, status }) => {
+      let updatedTrail = trails.find(
+        (trail) => id === trail.id
+      );
+      updatedTrail.status = status;
+      pubsub.publish("trail-status-change", {
+        trailStatusChange: updatedTrail
+      });
+      return updatedTrail;
+    }
+  },
+  Lift: {
+    trailAccess: (parent) =>
+      parent.trails.map((id) =>
+        trails.find((t) => id === t.id)
+      )
+  },
+  Trail: {
+    accessedByLifts: (parent) =>
+      parent.lift.map((id) =>
+        lifts.find((l) => id === l.id)
+      )
+  },
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "A valid date time value.",
+    parseValue: (value) => new Date(value),
+    serialize: (value) => new Date(value).toISOString(),
+    parseLiteral: (ast) => new Date(ast.value)
+  }),
+  Subscription: {
+    liftStatusChange: {
+      subscribe: (parent, data) =>
+        pubsub.asyncIterator("lift-status-change")
+    },
+    trailStatusChange: {
+      subscribe: (parent, data) =>
+        pubsub.asyncIterator("trail-status-change")
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: "/"
+});
+
+const serverCleanup = useServer({ schema }, wsServer);
+
+// Set up ApolloServer.
+const server = new ApolloServer({
+  schema,
+  context: { pubsub },
+  plugins: [
+    // Proper shutdown for the HTTP server.
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+
+    // Proper shutdown for the WebSocket server.
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          }
+        };
+      }
+    }
+  ]
+});
+
+await server.start();
+app.use(
+  "/",
+  cors(),
+  bodyParser.json(),
+  expressMiddleware(server)
+);
+
+const PORT = process.env.PORT || 4000;
+// Now that our HTTP server is fully set up, we can listen to it.
+httpServer.listen(PORT, () => {
+  console.log(
+    `Server is now running on http://localhost:${PORT}`
+  );
+});
